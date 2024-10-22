11b2d0c22d6d0768547b676ff6b61a5b5b63f507
Enrique Esquinas <esquinas.enrique@gmail.com>
2024-10-22 19:19:23 +0000

    Use query in controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,16 @@ class RecipesController < ApplicationController
 
   # GET /recipes
   def index
-    @recipes = Recipe.all.order("RANDOM()").take(5)
+    @all_ingredients = ["almond", "apple", "avocado", "bacon", "baking powder", "baking soda", "banana",
+      "beef", "bread", "butter", "cabbage", "cardamom", "carrot", "cayenne", "celery", "cheese", "chicken",
+      "chile", "chorizo", "cilantro", "cinnamon", "cod", "cooking spray", "corn", "cornstarch", "cumin", "dough",
+      "egg", "flour", "garlic", "ginger", "guacamole", "jalapeno", "ketchup", "lamb", "lemmon", "lettuce",
+      "mayo", "milk", "mustard", "oat", "oil", "onion", "oregano", "paprika", "parsley", "peanut", "peas", "pepper", "pork",
+      "radish", "raisin", "salmon", "salt", "sardine", "sausage", "sugar", "tofu", "tomato", "turkey",
+      "vanilla", "vinegar", "walnut", "water", "wine", "worcestershire sauce", "yeast", "yogurt"]
+    @ingredients = index_params.fetch(:q, [])
+
+    @recipes = Recipe::Query.resolve(@ingredients)
   end
 
   # GET /recipes/1
@@ -14,5 +23,9 @@ class RecipesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_recipe
       @recipe = Recipe.find(params.expect(:id))
+    end
+
+    def index_params
+      params.permit(q: [])
     end
 end
