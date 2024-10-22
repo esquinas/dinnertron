15f5234b3c148a2d64f65ea18b171b12eb8e9ffa
Enrique Esquinas <esquinas.enrique@gmail.com>
2024-10-22 10:52:05 +0000

    Write data seeding to use `rails db:seed`

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -10,4 +10,20 @@ class Recipe < ApplicationRecord
   # prep_duration (interval)
   # rating (float?)
   # title (string)
+
+  def self.from_scrape_data(data, null_authors: ["deleteduser"])
+    author = data[:author]
+    author = author.in?(null_authors) ? nil : author.presence
+    title = data.fetch(:title).strip
+
+    find_or_initialize_by(author:, title:) do |recipe|
+      recipe.category        = data[:category].presence
+      recipe.cook_duration   = data.fetch(:cook_time).to_i.minutes
+      recipe.cuisine         = data[:cuisine].presence
+      recipe.image           = data[:image].presence
+      recipe.ingredient_list = data.fetch(:ingredients).map { _1.presence }.compact
+      recipe.prep_duration   = data.fetch(:prep_time).to_i.minutes
+      recipe.rating          = data[:ratings].to_f
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "json"
+
+def show_progress(number)
+  Rails.logger.info("Seeding #{number} recipes ...") if (number % 500).zero?
+end
+
+recipes_dataset_file_path = Rails.root.join("db", "seeds", "recipes-en.json")
+
+unless recipes_dataset_file_path.exist?
+  STDERR.puts "DATASET WAS NOT FOUND IN dataset/#{recipes_dataset_file_path}"
+
+  exit(false)
+end
+
+recipes_dataset = JSON.parse(recipes_dataset_file_path.read, symbolize_names: true)
+
+recipes_dataset.each.with_index do |recipe_data, index|
+  Recipe.from_scrape_data(recipe_data).then do |recipe|
+    recipe.save! if recipe.valid? && recipe.new_record?
+  end
+
+  show_progress(index)
+end
